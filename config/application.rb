bd297903a7b5b7af3ad2c2f114166a16969bc5b0
Amy <trannycorrs@yahoo.com>
2017-08-03 19:32:32 +0000

    firster found

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Workspace
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
-    config.assets.paths << Rails.root.join("vendor","assets", "bower_components", "bootstrap-sass-official","assets", "fonts")
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf|woff2)\z/
+    #config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+    #config.assets.paths << Rails.root.join("vendor","assets", "bower_components", "bootstrap-sass-official","assets", "fonts")
+    #config.assets.precompile << /\.(?:svg|eot|woff|ttf|woff2)\z/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
