dcb5deca2b2a29b55fefa569683a1b2b5648b117
Amy <trannycorrs@yahoo.com>
2017-08-28 17:17:36 +0000

    datepicker autoclose

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,15 +11,15 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  #config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = false
   
-  if ENV['REDIS_URL']
-  config.action_controller.perform_caching = true
-  config.cache_store = :redis_store
-  else
-    config.cache_store = :redis_store, "redis://$IP:6379/1/ns", { expires_in: 90.minutes }
-    config.action_controller.perform_caching = true
-  end
+  #if ENV['REDIS_URL']
+  #config.action_controller.perform_caching = true
+  #config.cache_store = :redis_store
+  #else
+  #  config.cache_store = :redis_store, "redis://$IP:6379/1/ns", { expires_in: 90.minutes }
+  #  config.action_controller.perform_caching = true
+  #end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
