f8df8bb7c6b38c3ad7c4ce9205e7d4618baf2dce
Amy <trannycorrs@yahoo.com>
2017-09-06 18:51:53 +0000

    frontend materia

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'request#index'
+  root 'google_flights#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
    
