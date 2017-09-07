158105bc389d9c3ae95ab07710da53f863fc7419
Amy <trannycorrs@yahoo.com>
2017-09-07 16:52:43 +0000

    viewport adjusted

--- app/assets/javascripts/trips_app.js
+++ app/assets/javascripts/trips_app.js
@@ -122,7 +122,8 @@ tripapp.controller("TripSearchController",[ "$scope","$http",
       $scope.airports = response.data;
     }); 
     
-    $scope.people = [ "0","1","2","3","4","5","6","7","8","9","10"];
+    $scope.people = [ "1","2","3","4","5","6","7","8","9","10"];
+    $scope.children = [ "0","1","2","3","4","5","6","7","8","9","10"];
        
   }
 ]);
